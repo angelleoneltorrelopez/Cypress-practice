90155b2774ae9137a5515d63cd973337b3659be3
AngelTorre <angelleoneltorrelopez@gmail.com>
2023-03-30 23:56:03 -0600

    change performance score

--- cypress/e2e/web/lightHouse.cy.js
+++ cypress/e2e/web/lightHouse.cy.js
@@ -4,7 +4,7 @@ describe("SEO Test", () => {
   it("audits the home page", () => {
     cy.visit("https://www.ingenieriazeros.com/");
     const customThresholds = {
-      performance: 30,
+      performance: 10,
       accessibility: 50,
       "best-practices": 50,
       seo: 50
